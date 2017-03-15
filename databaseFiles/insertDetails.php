b80f49e2286e67f67529510c78dd5d462bc77c0b
Edwin Zakaria <medwin@opensuse.org>
2017-03-15 22:46:52 +0700

    ganti oauth ke opensuseid

--- databaseFiles/insertDetails.php
+++ databaseFiles/insertDetails.php
@@ -10,12 +10,13 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 //$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token, $access_token_secret);
 //$content = $connection->get("account/verify_credentials");
 
-$twitteruser = "maulblues";
+$twitteruser = "opensuseid";
 $notweets = 30;
-$consumerkey = "ZUMs0Hj1EgoROdRfjtXrSJkmp";
-$consumersecret = "XhWr8j2bGPehMPsclK7UXBcvjrVKxsSlwuUNf0PQlbqruqY95t";
-$accesstoken = "134039616-sYpKslNFqkzI0HNdapvmasuKyQrXwxYbGBnKQmNO";
-$accesstokensecret = "jQnMmTZ4YNqWcUrYYfrHdg2Y6WPAuP310kGHj2R1krZ7M";
+$consumerkey = "iQAwQV0sVg0TjYuNz4ENJ6r6d";
+$consumersecret = "FfzZRiHopE9T78PmXmNzDGb58Kd9l8irxjp7cpL1hr2BnpgYu8";
+$accesstoken = "740354983124574209-sNaPVEYtJngCMR6rKBI9dSpiFGG4Pd3";
+$accesstokensecret = "qefFkA4Mq1PtaAE4nim69GRLr9H4Vl4eCyMG15nJvN4U4";
+
  
 function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
   $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
